d453c93f06115c0aa680b930a190e8b47367ba2b
Mike Jovanovich <mpjovanovich@gmail.com>
2025-07-03 23:42:09 -0400

    initial create

---
+++ db_init/02-data.sql
@@ -0,0 +1,12 @@
+USE test;
+
+INSERT INTO students(name, email) VALUES
+('Annie Aligator', 'aaa@aaa.org'),
+('Bobby Beebop', 'bbb@bbb.org'),
+('Cassie Carp', 'ccc@ccc.org'),
+('Craig Camel', 'ccc@ccc.com');
+
+INSERT INTO courses(name, credits) VALUES
+('INFM 109', 3),
+('SDEV 120', 3),
+('FUN 999', 9);
